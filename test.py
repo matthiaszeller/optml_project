c3d3969612dd263e109745b4809b54f16a316e57
Matthias Zeller <m.zeller1901@gmail.com>
2021-05-13 18:01:53 +0200

    docstrings, comments

--- test.py
+++ test.py
@@ -30,28 +30,40 @@ class NesterovOptimizer(Optimizer):
         super().__init__(params, defaults)
 
     def step(self, closure: Optional[Callable[[], float]] = ...) -> Optional[float]:
+        # State stores the variables used by optimization algo
+        # need to initialize it if empty
         if len(self.state) == 0:
             self.__initialize_state()
 
         step = self.state['general']['step']
+        # Iterate over parameter groups (only 1 group in our case)
         for gp in self.param_groups:
             lr = gp['lr']
             params = gp['params']
+            # Iterate over parameters in the group
             for p in params:
                 if p.grad is None:
                     continue
 
                 pstate = self.state[p]
+                # Implementation of Nesterov algorithm
                 pstate['y'] = p.data - lr * p.grad
                 pstate['z'] = pstate['z'] - (step + 1) * lr / 2 * p.grad
                 p.data = (step + 1) / (step + 3) * pstate['y'] + 2 / (step + 3) * pstate['z']
 
+        # Increment step
         self.state['general']['step'] += 1
 
     def __initialize_state(self):
+        """Initialize state variables:
+            - there's a 'general' state in which we store the steps
+            - there's a state for each model parameter
+        """
         self.state['general']['step'] = 0
         for gp in self.param_groups:
+            # Initialize parameter-specific state
             for p in gp['params']:
+                # But only for parameters having a gradient
                 if p.grad is None:
                     continue
 

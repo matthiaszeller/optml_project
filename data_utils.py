673bcd4734c640659e72cf35ea51a3e0dfac4863
Matthias Zeller <m.zeller1901@gmail.com>
2021-05-24 19:07:13 +0200

    fix build loaders

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Tuple
 
+import numpy as np
 import torch
 import torchvision
 from torch.utils.data import DataLoader, TensorDataset
@@ -38,10 +39,36 @@ def get_mnist(normalize=True, path='data'):
 
 def build_data_loaders(train_dataset, test_dataset, batch_size) -> Tuple[DataLoader, DataLoader]:
     # Add an axis to have one channel
-    train_dataset.data = train_dataset.data[:, None, :]
-    test_dataset.data = test_dataset.data[:, None, :]
-
-    train_loader = DataLoader(TensorDataset(train_dataset.data, train_dataset.targets), batch_size=batch_size)
-    test_loader = DataLoader(TensorDataset(test_dataset.data, test_dataset.targets), batch_size=batch_size)
+    train_loader = DataLoader(TensorDataset(train_dataset.data[:, None, :], train_dataset.targets), batch_size=batch_size)
+    test_loader = DataLoader(TensorDataset(test_dataset.data[:, None, :], test_dataset.targets), batch_size=batch_size)
 
     return train_loader, test_loader
+
+
+def get_height_weight_gender(sub_sample=True, add_outlier=False):
+    """Load height-weight-gender data and convert it to the metric system."""
+    path_dataset = "data/height_weight_genders.csv"
+    data = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[1, 2])
+    height = data[:, 0]
+    weight = data[:, 1]
+    gender = np.genfromtxt(
+        path_dataset, delimiter=",", skip_header=1, usecols=[0],
+        converters={0: lambda x: 0 if b"Male" in x else 1})
+    # Convert to metric system
+    height *= 0.025
+    weight *= 0.454
+
+    # sub-sample
+    if sub_sample:
+        height = height[::50]
+        weight = weight[::50]
+        gender = gender[::50]
+
+    if add_outlier:
+        # outlier experiment
+        height = np.concatenate([height, [1.1, 1.2]])
+        weight = np.concatenate([weight, [51.5/0.454, 55.3/0.454]])
+
+    return height, weight, gender
+
